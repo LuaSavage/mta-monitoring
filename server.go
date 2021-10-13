bd1f6c7fb68fe6ae2518f1fa206e046465cddfe1
russianSlave <ilya.shikri@gmail.com>
2021-10-14 01:52:32 +0300

    !

--- server.go
+++ server.go
@@ -1,16 +1,16 @@
 package server
 
 import "fmt"
- 
+
 type Server struct {
-	timeout float64
-    game string
-    port int
-    name string
-    gamemode string
-    map_name string
-    version  string
-    somewhat string
-    players int
-    maxplayers int
+	timeout    float64
+	game       string
+	port       int
+	name       string
+	gamemode   string
+	map_name   string
+	version    string
+	somewhat   string
+	players    int
+	maxplayers int
 }
