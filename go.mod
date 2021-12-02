78d24dd3709f62aff361e8a9631d841f6d5b0cea
russianSlave <ilya.shikri@gmail.com>
2021-12-03 00:54:53 +0300

    some attempt to write tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mta-monitoring
 
 go 1.16
 
-require github.com/davecgh/go-spew v1.1.1 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ test/server_test.go
@@ -0,0 +1,20 @@
+package server_test
+
+import (
+	//"mta-monitoring/server"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+
+  // assert equality
+  assert.Equal(t, 123, 123, "they should be equal")
+
+  // assert inequality
+  assert.NotEqual(t, 123, 456, "they should not be equal")
+
+
+}
+
+
